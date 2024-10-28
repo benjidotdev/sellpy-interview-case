0f6894cc92f3ba91fde17f7beb0ea776e2b2f851
Benji <benji@benji.dev>
2024-10-29 00:56:06 +0100

    update list schema so that a todo always belongs to a list

--- backend/src/models/todo.model.js
+++ backend/src/models/todo.model.js
@@ -3,6 +3,11 @@ import mongoose from 'mongoose';
 const todoSchema = new mongoose.Schema({
   description: {
     type: String,
+  },
+  list: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'List',
+    required: true,
   }
 }, {
   timestamps: true
