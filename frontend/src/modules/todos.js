57fda68f9b2a8dcff4444d4dfbf2307d560ac4ec
Benji <benji@benji.dev>
2024-10-29 12:25:59 +0100

    prevent empty todos being saved

--- frontend/src/modules/todos.js
+++ frontend/src/modules/todos.js
@@ -1,9 +1,11 @@
 import { fetchApi } from './fetchApi.js';
 
 export const saveListTodos = async ({ listId, todos }) => {
+  const filteredTodos = todos.filter((todo) => todo.description.trim() !== '');
+  if (filteredTodos.length === 0) return;
   const url = `list/${listId}/todos`;
   const method = 'PUT';
-  const data = todos;
+  const data = filteredTodos;
   return await fetchApi(url, method, data);
 };
 
